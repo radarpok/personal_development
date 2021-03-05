22aa645fb3ce2091eb8b1e05347cdefd5cec0bd3
radarpok <vdiana.m8@gmail.com>
2021-03-05 21:15:25 +0100

    rewriting 11 destructive habit to healthy ones

---
+++ felulirva.js
@@ -0,0 +1,57 @@
+export const felulirni = [
+    {
+        id: 1,
+        felulirando: 'Borúlátás és jövőkép-elképzelési-zavar, amikor igazságtalanságot élek meg',
+        felulirva: 'Fizikai helyváltoztatás, utazás akár, hogy a perspektívaváltás(-és szélesítés) katalizálja a megértési folyamatot'
+    },
+    {
+        id: 2,
+        felulirando: 'Figyelem fókuszának zavara, amikor egyszerre akarok minden célomért tenni valamit',
+        felulirva: 'Abba belekezdeni, ami a legaktuálisabb, legsürgősebb, előttem van, delegálás ha szükséges; apránként haladni, kis lépésekkel kezdeni!'
+    },
+    {
+        id: 3,
+        felulirando: 'Túltervezés (a cselekvés rovására)',
+        felulirva: 'Megfogalmazom / elképzelem / leírom egészen pontosan, hogy mit szeretnék elérni (anyagiakban, egészségben, érzelmekben, spiritualitásban), és a jelek maguktól értetődően fognak elvezetni odáig (https://drive.google.com/file/d/1TbRH-8_GGj5LNj0mndQPsQNy-71o_w13/view?usp=sharing)'
+    },
+    {
+        id: 4,
+        felulirando: 'Sértettség-érzetre elégtételvétellel (szarkazmussal / távolságtartással / igazság-bizonygatással) reagálni',
+        felulirva: 'Levetkőzni a beépült "sértődöttnek kell mutatnom magam hogy kifejezzem mennyire értékesnek tartom az adott egyént" szokást; Gyakorlatban: nagy levegőt venni, elszámolni 10-ig, kimondani magamban: a szeretetet, a boldogságot, a tökéletességet választom'
+    },
+    {
+        id: 5,
+        felulirando: 'Szőrszálhasogatásig elemezni dolgokat, míg rám nem jön a feszültségérzet',
+        felulirva: 'Fizikai munkát végezni, a többit rábízni a tudatfelettire (https://drive.google.com/file/d/1xYftgi0D1sjcINzVAChojgjXKpTKDFgR/view?usp=sharing)'
+    },
+    {
+        id: 6,
+        felulirando: 'Aprólékos pontossággal beszabályozni, hogy valaki mit és hogyan csináljon',
+        felulirva: 'Irányítás helyett vezetés példamutatás által; Nyugalmas, támogató légkör megteremtése és csak a cél kitűzése a mód(szer) helyett'
+    },
+    {
+        id: 7,
+        felulirando: 'Gyakori elkalandozás, merengés + kiszámítható körülmények meglétének szükségérzete ahhoz, hogy mindenre választ találjak',
+        felulirva: 'Feltenni magamban a pontos kérdést, majd nem gondolni rá, hagyni hogy kikristályosodjon a válasz; A pontos kérdés megfogalmazásához: programot adni az elemeknek mos(d)ás közben, lélegzés közben, tűzgyújtás közben, kertrendezés közben hogy megértve mindent, ami eddig rombolt az áldássá váljon bennem, összesen MAX 10 percet szánni a gondolat fenntartására naponta!'
+    },
+    {
+        id: 8,
+        felulirando: 'Túlérzékeny "piroslámpa", féltés, óvás egy rövid és enyhe gyermeki "lelki láz" miatt is',
+        felulirva: 'Humor :) / Higgadt cselekvés, kongruencia. Odafigyelni az egészséges szerotonin- és kortizolszintre, a hipotalamusz és az amygdala megfelelő működésére, valamint a munka és az önjutalmazás egyensúlyára'
+    },
+    {
+        id: 9,
+        felulirando: 'Erőltetett "napos oldal-keresés" a pillanatnyi érzelmek ki-nem-fejezése árán',
+        felulirva: 'Megélni és kimondani ahogyan érzem magam, majd nézőpontot váltani, nem tartva attól, hogy "beleragadok" az aktuális érzelmi állapotba azzal, hogy kifejezem'
+    },
+    {
+        id: 10,
+        felulirando: 'Magányos farkas üzemmód, elszigetelődés',
+        felulirva: 'Minden kapcsolatban van mindennel, ezért az én problémám más problémája is, tehát nincs értelme rejtegetni vagy másokra mutogatni az okáért -> felvállalni és együttműködni többedmagammal a megoldásában'
+    },
+    {
+        id: 11,
+        felulirando: 'Önromboló objektív "kívülálló" vagy épp szubjektív "szenvedő alany" hozzáállás',
+        felulirva: 'Merni hibázni, mert minél többször érzem, hogy mindenből talpra tudok állni, annál inkább kitisztul az intuícióm hangja és gondolkodás nélkül tudom, hogy az adott pillanatra hogyan reagáljak'
+    }
+]
