46e11fe746f381d86ae497f88d50643ee35144cd
radarpok <vdiana.m8@gmail.com>
2021-03-06 12:36:12 +0100

    adding a dramatic liver detox recipe for maximum cleansing effect

---
+++ tisztitokura.js
@@ -0,0 +1,25 @@
+export const tisztitokura = [
+    {   // úgy tervezni, hogy aznap és másnap ne kelljen sehova sem utaznom és szabad legyen a wc :D
+        nap: 1,
+        recept_1: '10dl vízben feloldok 100g keserűsót',
+        // a bevitel előtt max. 4 órával elkészíteni, mert nem tesz jót az olívaolajnak, ha hűtőben van tárolva
+        recept_2: 'egy grapefruit levét kifacsarom, összekeverem 1dl extra szűz olíva olajjal + 05 dl máriatövis olajjal + 05 dl lenmag olajjal',
+        teendo: {
+            14.00: 'eddig az időpontig könnyebb ételeket (pl. zöldségleves) fogyasztok, addig is sok folyadékot iszom; ezután nem eszem semmit',
+            16.00: 'eddig az időpontig letolok 2dl keserűsós oldatot',
+            18.00: 'második adag 2dl keserűsós oldat + elkészítem a grapefruit + olajok kombinációt',
+            20.00: 'harmadik adag 2dl keserűsós oldat',
+        // az alvás ideje alatt enyhe görcsökkel kell számolnom
+            22.00: 'kortyonként lassan megiszom a grapfruit + olajok kombinációt, 5-15 perc alatt, majd azonnal lefekszem a jobb oldalamra',
+        }
+    },
+    {
+        nap: 2,
+        teendo: {
+            8.00: 'negyedik adag 2dl keserűsó oldat + kicsit visszapihenni',
+            10.00: 'utolsó adag 2dl keserűsó oldat',
+            11.00: '2dl gyümölcslé, fél óra múlva már gyümölcsöt is lehet enni + innentől egy hónapig jó minőségű probiotikum',
+            12.00: 'könnyebb ételeket már lehet ennem, vacsorára teljesen rendben jövök'
+        }
+    }
+]
